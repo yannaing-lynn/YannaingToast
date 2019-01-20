6095e9e492c62ade6d1a14fea1be2882f8d0ae40
Yan Naing Lynn <rghtbossy@gmail.com>
2019-01-20 20:44:19 +0630

    Update Version 1.1

--- app/src/main/java/com/orange/librarytest/MainActivity.java
+++ app/src/main/java/com/orange/librarytest/MainActivity.java
@@ -1,15 +1,17 @@
 package com.orange.librarytest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.yannainglynn.toast.YannaingToast;
 
 public class MainActivity extends AppCompatActivity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        YannaingToast yannaingToast = new YannaingToast(this);
+        yannaingToast.simpleToast(getLayoutInflater(),"How are you?");
     }
 
 }
--- yntoast/src/main/java/com/yannainglynn/toast/YannaingToast.java
+++ yntoast/src/main/java/com/yannainglynn/toast/YannaingToast.java
@@ -3,38 +3,35 @@ import android.content.Context;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class YannaingToast {
     Context context;
-    LayoutInflater layoutInflater;
-
-    public YannaingToast(Context context, LayoutInflater layoutInflater) {
+    public YannaingToast(Context context) {
         this.context = context;
-        this.layoutInflater = layoutInflater;
     }
-
     // My custom toast
-    public void btntoast(View view) {
+    public void simpleToast(LayoutInflater layoutInflater,String string) {
         Context context=this.context;
         LayoutInflater inflater=layoutInflater;
-
         View customToastroot =inflater.inflate(R.layout.mytoast, null);
-
+        TextView textView = customToastroot.findViewById(R.id.simpleToast);
+        textView.setText(string);
         Toast customtoast=new Toast(context);
-
         customtoast.setView(customToastroot);
         customtoast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL,0, 0);
         customtoast.setDuration(Toast.LENGTH_LONG);
         customtoast.show();
     }
     // Red toast
-    public void btnRed(View view) {
+    public void redToast(LayoutInflater layoutInflater,String string) {
         Context context=this.context;
         LayoutInflater inflater=layoutInflater;
 
         View customToastroot =inflater.inflate(R.layout.red_toast, null);
-
+        TextView tvRedToast = customToastroot.findViewById(R.id.tvRedToast);
+        tvRedToast.setText(string);
         Toast customtoast=new Toast(context);
 
         customtoast.setView(customToastroot);
@@ -43,12 +40,13 @@ public class YannaingToast {
         customtoast.show();
     }
     // Yellow toast
-    public void btnYellow(View view) {
+    public void yellowToast(LayoutInflater layoutInflater,String string) {
         Context context=this.context;
         LayoutInflater inflater=layoutInflater;
 
         View customToastroot =inflater.inflate(R.layout.yellow_toast, null);
-
+        TextView tvYellowToast = customToastroot.findViewById(R.id.tvYellowToast);
+        tvYellowToast.setText(string);
         Toast customtoast=new Toast(context);
 
         customtoast.setView(customToastroot);
@@ -57,12 +55,12 @@ public class YannaingToast {
         customtoast.show();
     }
     // Blue toast
-    public void btnBlue(View view) {
+    public void blueToast(LayoutInflater layoutInflater,String string) {
         Context context=this.context;
         LayoutInflater inflater=layoutInflater;
-
         View customToastroot =inflater.inflate(R.layout.blue_toast, null);
-
+        TextView tvBlueToast = customToastroot.findViewById(R.id.tvBlueToast);
+        tvBlueToast.setText(string);
         Toast customtoast=new Toast(context);
 
         customtoast.setView(customToastroot);
